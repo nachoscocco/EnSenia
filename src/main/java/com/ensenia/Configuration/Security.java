fedabd562a94dc9c081ef356525533124001e728
Pablo Rosso <pablo.rosso.n@hotmail.com>
2021-11-01 18:50:28 -0300

    UsuarioServicio

--- src/main/java/com/ensenia/Configuration/Security.java
+++ src/main/java/com/ensenia/Configuration/Security.java
@@ -1,18 +1,17 @@
-<<<<<<< HEAD
 
  
 package com.ensenia.Configuration;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 
@@ -51,57 +50,7 @@ public class Security extends WebSecurityConfigurerAdapter {
 
     }
 }
-=======
+
 
  
-package com.ensenia.Configuration;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-
-
-@Configuration
-@Order(1)
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class Security extends WebSecurityConfigurerAdapter {
-
-//    @Autowired
-//    public UsuarioServicio usuarioServicio;
-    
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth)throws Exception{
-//        auth
-//                .userDetailsService(usuarioServicio)
-//                .passwordEncoder( new BCryptPasswordEncoder());
-//    }
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.headers().frameOptions().sameOrigin().and()
-                .authorizeRequests()
-                        .antMatchers("/css/*", "/js/*","/img/*", "/**").permitAll()
-                .and().formLogin()
-                        .loginPage("/login") // Que formulario esta mi login
-                                .loginProcessingUrl("/logincheck")
-                                .usernameParameter("username") // Como viajan los datos del logueo
-                                .passwordParameter("password")// Como viajan los datos del logueo
-                                .defaultSuccessUrl("/inicio") // A que URL viaja
-                                .permitAll()
-                        .and().logout() // Aca configuro la salida
-                                .logoutUrl("/logout")
-                                .logoutSuccessUrl("/")
-                                .permitAll().and().csrf().disable();
-
-    }
-}
->>>>>>> master
--- src/main/java/com/ensenia/Controller/mainController.java
+++ src/main/java/com/ensenia/Controller/mainController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 package com.ensenia.Controller;
 
@@ -6,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
 public class mainController {
     
     @GetMapping("/")
@@ -23,29 +21,4 @@ public class mainController {
     
     
 }
-=======
 
-package com.ensenia.Controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-
-public class mainController {
-    
-    @GetMapping("/")
-    public String index() {
-        return "index.html";
-    }
-    
-
-    @GetMapping("/inicio")
-    public String inicio() {
-        return "inicio.html";
-    }
-    
-    
-    
-}
->>>>>>> master
