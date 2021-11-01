1cf6795d377fc76b0dedbf85632cb219cec70d9c
nachoscocco <nachoscocco14@gmail.com>
2021-11-01 19:04:27 -0300

    modif pablo fin

--- src/main/java/com/ensenia/Error/ErrorServicio.java
+++ src/main/java/com/ensenia/Error/ErrorServicio.java
@@ -1,9 +1,9 @@
 
 package com.ensenia.Error;
 
-import org.springframework.stereotype.Service;
 
-@Service
+
+
 public class ErrorServicio extends Exception{
  
     public ErrorServicio(String msn){
